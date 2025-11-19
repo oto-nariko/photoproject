469f0049b0b23c8842280bfeb24a02e57ed03dcd
kotoka <fko2547055@stu.o-hara.ac.jp>
2025-11-19 10:05:43 +0900

    11/19マイグレーション済み

--- photoproject/photoproject/settings.py
+++ photoproject/photoproject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'photo.apps.PhotoConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -100,6 +101,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
 
 
 # Internationalization
